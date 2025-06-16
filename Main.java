20e6fa4870d5e25463404a8a48ede8f73f4e114e
kumararunesh <kumararunesh211994@gmail.com>
2025-06-16 12:49:49 +0530

    Added java file

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Main{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World from java");
+                System.out.println("Output from java program");
+                System.out.println(new Date());
+
+    }
+}
